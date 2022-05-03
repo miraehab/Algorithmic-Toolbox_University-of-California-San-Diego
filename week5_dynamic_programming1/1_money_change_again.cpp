4ec0291f50fde8d5a1ce76e0d0ac3ca134e0c86c
Mira Ehab <miraeha2@gmail.com>
2022-05-03 11:41:47 +0200

    Solved problem 1 in week 5

--- week5_dynamic_programming1/1_money_change_again.cpp
+++ week5_dynamic_programming1/1_money_change_again.cpp
@@ -1,12 +1,33 @@
 #include <iostream>
+#include <vector>
+#include <climits>
+#include <algorithm>
+
+using namespace std;
+
+int coins[3] = {1, 3, 4};
 
 int get_change(int m) {
-  //write your code here
-  return m / 4;
+  vector<int> total(m + 1, INT_MAX);
+	total[0] = 0;
+
+	for(int i = 1; i <= m; ++i){
+		for(int j = 0; j < 3; ++j){
+			if(i >= coins[j]){
+				int prev = total[i - coins[j]];
+
+				if(prev != INT_MAX){ 
+          total[i] = min(total[i], prev + 1);
+        }
+			}
+		}
+	}
+
+	return total[m];
 }
 
-int main() {
+int main(){
   int m;
-  std::cin >> m;
-  std::cout << get_change(m) << '\n';
+  cin >> m;
+  cout << get_change(m) << '\n';
 }
